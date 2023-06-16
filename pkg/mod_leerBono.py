75351f40dbbb6891ec8c5cdccfb23176460426b4
Fredy Yecid Castro Agray <fredy.castro@gmail.com>
2023-06-16 06:23:37 -0500

    utlziar loc para las condiciones de datos

--- pkg/mod_leerBono.py
+++ pkg/mod_leerBono.py
@@ -6,9 +6,10 @@ Created on Thu May  4 06:24:23 2023
 @author: Fredy Yecid Castro Agray
 """
 import pandas as pd
-import numpy as np
+#import numpy as np
 
 def leerBono( archivoLeeBono, fecha ):
+    # Crea dataset con pandas para trabajar los datos del archivo
     datosBono = pd.read_csv(archivoLeeBono, sep=";", header = 0 )
    
     # print(datosBono.head())
@@ -20,39 +21,27 @@ def leerBono( archivoLeeBono, fecha ):
                                       , format="%d/%m/%Y")
     print('Fecha a procesar',fecha)
     print ('TRN_DATE-->', datosBono['TRN_DATE'].dtypes)
-    
-    # (df3[(df3.county=='Manhattan') & (df.party=='Democrat')])
+
     # Quitamos los registros menores e iguales a la fecha de proceso 
-    datosBono = datosBono[(datosBono['TRN_DATE']<= fecha)]
+    datosBono = datosBono.loc[(datosBono['TRN_DATE']<= fecha)]
     print('Despues de <=', datosBono.shape[0])
+    
     #Quita los espacios en el campo M_TP_PFOLIO
     datosBono['M_TP_PFOLIO'] = datosBono['M_TP_PFOLIO'].str.split(expand=True)
+    
     # solo dejamos en el dataset los siguientes portafolios
     datosBono = datosBono[datosBono.M_TP_PFOLIO.isin(
         ('FI1','FI2','FI3','FI4','FI6','FI7_SUBAST',''))]
     print('Despues de portafolios ->', datosBono.shape[0])
-    
-    # df.dropna(subset=['name', 'toy']) 
+  
     # los stldate vacios se quitan del data set 
     datosBono = datosBono.dropna(subset=['SETTLEMENT_DATE'])
     print('Despues de quitar nulos ->', datosBono.shape[0])
 
-    # df['diff_days'] = (df['end'] - df['start']) / np.timedelta64(1, 'D')
-    #df['Resultado'] = df['A'] - df['B']
-    # genera solo los mayores con esta resta 
-    datosBono = (datosBono['SETTLEMENT_DATE'] > datosBono['TRN_DATE'])  # Revisar loc ejemplos 
+    # genera solo los mayores 
+    datosBono = datosBono.loc[(datosBono['SETTLEMENT_DATE'] > datosBono['TRN_DATE'])]  
     print('Mayor fecha ->', datosBono.shape[0])
 
 
-    # |                          (datosBono['SETTLEMENT_DATE']<= fecha)]
-    #print(datosBono)      
-    # Quitar los espacios para poder realizar la comparacion .split
 
-    # print(datosBono['SETTLEMENT_DATE'])
-    # print(datosBono.head())
-  
-    # datosBono['SETTLEMENT_DATE'] = pd.to_datetime(datosBono['SETTLEMENT_DATE']
-    #                                   , format="%d/%m/%Y")
-    # print(datosBono['SETTLEMENT_DATE'] - fecha )
-    # print(fecha)
-  
+    #print(datosBono)      
